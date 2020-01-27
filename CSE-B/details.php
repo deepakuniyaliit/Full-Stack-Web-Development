7cb816beea8b0c717bfe3ff396e4217bbc7131b5
Deepak Uniyal <deepak.uniyal08@gmail.com>
2020-01-27 12:00:18 +0530

    insert data into database

--- CSE-B/details.php
+++ CSE-B/details.php
@@ -4,12 +4,15 @@ include('config.php');
 
 <?php 
 if (isset($_POST['btn'])){
- echo "Dear, ".$_POST['username']." Your details are".'<br>';
- echo 'ID - '.$_POST['id'].'<br>';
- echo 'Username - '.$_POST['username'].'<br>';
- echo 'Age - '.$_POST['age'].'<br>';
- echo 'Contact - '.$_POST['contact'].'<br>';
- echo 'Email - '.$_POST['email']; 
+ echo "Dear, ".$_POST['username']." You are Welcome.".'<br>';
+ $username = $_POST['username'];
+ $age = $_POST['age'];
+ $contact = $_POST['contact'];
+ $email = $_POST['email']; 
+    
+    $sql  = "INSERT INTO `users` (name, age, contact, email) 
+    VALUES ('$username', '$age', '$contact', '$email')";
+    mysqli_query($conn, $sql);
 }
 else{
     echo "Welcome Guest";
--- CSE-B/phpInput.php
+++ CSE-B/phpInput.php
@@ -5,7 +5,6 @@
     
     <body>
     <form action="details.php" method="post">
-      ID: <input name="id", type="text" placeholder="Enter ID"><br>
       Name: <input type="text" name="username"><br>
       Age:<input type="text" name="age" placeholder="Enter Age"><br>
       Contact:<input type="text" name="contact" placeholder="Enter Age"><br>
