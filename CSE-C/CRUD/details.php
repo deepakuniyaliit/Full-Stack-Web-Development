39fe21cdd2f7b9328641c25c274cd26a82c091b1
Deepak Uniyal <deepak.uniyal08@gmail.com>
2020-02-03 10:33:20 +0530

    fetch data from database using PHP script

--- CSE-C/CRUD/details.php
+++ CSE-C/CRUD/details.php
@@ -1,12 +1,18 @@
 <?php
-if(isset($_POST['submit'])){
-    echo 'ID - '.$_POST['id'].'<br>';
-    echo 'UserName - '.$_POST['username'].'<br>';
-    echo 'Age - '.$_POST['age'].'<br>';
-    echo 'Contact - '.$_POST['contact'].'<br>';
-    echo 'Email - '.$_POST['email'].'<br>';
-}
-else{
-    echo "Welcome, Guest!";
+include('config.php');
+?>
+
+<?php
+$sql = "SELECT * FROM `users`";
+$result = mysqli_query($conn, $sql);
+
+if ($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+     echo $row['id'].' ';
+     echo $row['name'].' ';
+     echo $row['age'].' ';
+     echo $row['contact'].' ';
+     echo $row['email'].'<br>';
+    }
 }
 ?>
