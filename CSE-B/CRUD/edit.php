dd0d8a8e2ccad035f0fa9ca5d99b440673587b0b
Deepak Uniyal <deepak.uniyal08@gmail.com>
2020-02-05 13:13:18 +0530

    code to edit a record

--- CSE-B/CRUD/edit.php
+++ CSE-B/CRUD/edit.php
@@ -1,7 +1,36 @@
 <?php
-$id = $_GET['id'];
-echo "Id to be updated is - ".$id;
+include('config.php');
+?>
+<?php $id=$_GET['id'];
+?>
 
+<?php
+    $sql = "SELECT * FROM `users` WHERE id='$id'";
+    $result = mysqli_query($conn, $sql);
+    $data = $result->fetch_assoc();
+    $username = $data['name'];
+    $age = $data['age'];
+    $contact = $data['contact'];
+    $email = $data['email'];
+?>
+
+<?php
+if(isset($_POST['btn'])){
+    $username = $_POST['username'];
+    $age = $_POST['age'];
+    $contact = $_POST['contact'];
+    $email = $_POST['email'];
+    $sql = "UPDATE `users` SET name='$username', age='$age', contact='$contact', email='$email' WHERE id='$id'";
+    if(mysqli_query($conn, $sql)){
+        echo "Data updated successfully...";
+    }
+    else{
+        echo "Updation Failed...Try Again";
+    }
+}
+else{
+    echo "Please click update button";
+}
 ?>
 
 <html>
@@ -10,12 +39,15 @@ echo "Id to be updated is - ".$id;
     </head>
     
     <body>
-    <form action="edit.php" method="post">
-      Name: <input type="text" name="username"><br>
-      Age:<input type="text" name="age" placeholder="Enter Age"><br>
-      Contact:<input type="text" name="contact" placeholder="Enter Age"><br>
-      Email: <input type="email" name="email" placeholder="Enter Age"><br>
-      <input type="submit" name='btn'>
+    <form action="edit.php?id=<?php echo $id ?>" method="post">
+      Name: <input type="text" name="username" value="<?php echo $username; ?>"><br>
+      Age:<input type="text" name="age"  value="<?php echo $age; ?>" placeholder="Enter Age"><br>
+      Contact:<input type="text" name="contact" value="<?php echo $contact; ?>" placeholder="Enter Age"><br>
+      Email: <input type="email" name="email" value="<?php echo $email; ?>" placeholder="Enter Age"><br>
+      <input type="submit" name='btn' value="Update">
     </form>
     </body>
 </html>
+
+
+58, 61, 64
