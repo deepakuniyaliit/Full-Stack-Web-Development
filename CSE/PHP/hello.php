4b49389ce81fca144349950fd67a37e3929a6881
Deepak Uniyal <deepak.uniyal08@gmail.com>
2020-06-02 21:06:02 +0530

    create hello world in PHP

---
+++ CSE/PHP/hello.php
@@ -0,0 +1,14 @@
+<html>
+<h1>My First PHP Code</h1>
+<?php
+    echo "Hello World!<br>";
+    ECHO "Hello World!<br>";
+    EcHo "Hello World!<br>";
+?>
+
+<?php
+    $name = "PHP";
+    echo "Programming Language - ".$name." is very Easy<br>";
+    echo "Programming Language - ".$NAME." is very Easy<br>";
+?>
+</html>
